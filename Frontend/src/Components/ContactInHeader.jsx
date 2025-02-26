c73c5175585865558066767cac1cf4e10487d2bb
krishhackz <krishhackz.in@gmail.com>
2025-02-26 17:12:14 +0530

    create Contact component in header

---
+++ Frontend/src/Components/ContactInHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+function ContactInHeader() {
+  return (
+     <>
+        <Header/>
+        <div class="pt-14">
+        <Footer/>
+        </div>
+        </>
+  )
+}
+
+export default ContactInHeader
